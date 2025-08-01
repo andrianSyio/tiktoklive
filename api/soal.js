92494a665b06aaa13460ed0ef3608235676eb317
Aris Zulwafa <119752803+andrianSyio@users.noreply.github.com>
2025-08-01 12:30:48 +0700

    Update soal.js

--- api/soal.js
+++ api/soal.js
@@ -1,6 +1,10 @@
-import soalData from '../../data/soal.json';
+import fs from 'fs';
+import path from 'path';
 
 export default function handler(req, res) {
+  const soalPath = path.resolve('./data/soal.json');
+  const soalData = JSON.parse(fs.readFileSync(soalPath, 'utf-8'));
   const acak = soalData[Math.floor(Math.random() * soalData.length)];
+
   res.status(200).json(acak);
 }
